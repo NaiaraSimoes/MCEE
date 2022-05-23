50fbb1facbac631d99229913c165c9814b539ab5
Naiara Simões <57753296+NaiaraSimoes@users.noreply.github.com>
2022-05-23 15:18:10 +0100

    Update metNewton.m

--- metNewton.m
+++ metNewton.m
@@ -31,20 +31,19 @@ title('f(x) = ',e);
 xlabel('xx'); ylabel('yy');
 plot(x, x*0, 'k-');
 
-% verificacao de solução 
+% verificação de solução 
 if min(fx) < 0 && max(fx) > 0 %se o menor resultado for negativo e o maior 
                               %positivo, então há um valor nulo pra equação
+    tabela(fx,xx);
     m = '0';
     while m == '0'
-        animation(fx, xx); 
-        tabela(fx,xx);
+        animation(fx, xx);
        
         m = input('Aperte 1 para VOLTAR AO MENU: ','s');
         if m ~= '1'
             clc;
             m = '0';
-        end
-        
+        end 
     end    
 else
     fprintf('Equação sem solução.\nPressione qualquer tecla para voltar ao menu.\n');
