8d4c563407aba47757c7038c3183bdb10ae20eb2
Victor277 <victorkbem@hotmail.com>
2022-05-23 16:19:07 +0100

    Update metSecante.m

--- metSecante.m
+++ metSecante.m
@@ -30,34 +30,47 @@ end
 clc;
 fprintf('MÉTODO DAS SECANTES: f(x) = %s\n', e);
 fprintf('Posições iniciais: %.2f | %.2f\n', x1, x2);
+fprintf('Número máximo de repetições: %d\n', n);
+fprintf('Distância mínima aceita: %.4f\n', tol);
 
+m = '0';
 [xx, fx] = secante(f, x1, x2, n, tol);
 
 
 % gráfico:
 x = linspace(-max(abs(xx)), max(abs(xx)), 1000);
 plot(x,f(x));grid on; hold on;
-title('f(x) = ',e);
+title('f(x) = ');
 xlabel('xx'); ylabel('yy');
 plot(x, x*0, 'k-');
 
 
 % voltar ao menu
-if min(fx) <= tol && max(fx) > 0 %se o menor resultado for negativo e o maior
-                              %positivo, então há um valor nulo pra equação
-    tabela(fx, xx);
-    m = '0';
+if length(xx)<n
     while m ~= '1'
-        animation(fx, xx);
-        
-        m = input('Aperte 1 para VOLTAR AO MENU: ','s');
-        if m ~= '1'
-            clc;
-            m = '0';
-        end       
-    end    
+        clc;
+        fprintf('MÉTODO DAS SECANTES: f(x) = %s\n', e);
+        tabela(f, xx, length(xx));
+        for r = 1:3
+            animation(fx, xx);
+        end
+        m = input('PRIMA 1 PARA VOLTAR AO MENU: ', 's');
+    end
+    
+elseif min(fx) <= tol && max(fx) > 0 %se o menor resultado for negativo e o maior
+    %positivo, então há um valor nulo pra equação
+    while m ~= '1'
+        clc;
+        fprintf('MÉTODO DAS SECANTES: f(x) = %s\n', e);
+        tabela(f, xx, length(xx));
+        for r = 1:3
+            animation(fx, xx);
+        end
+        m = input('PRIMA 1 PARA VOLTAR AO MENU: ', 's');
+    end
 else
     fprintf('Equação sem solução.\nPressione qualquer tecla para voltar ao menu.\n');
     pause;
-end   
+end
+    
 menu
