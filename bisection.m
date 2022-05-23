62db729afd2c73c1fffa7df016dc258f40b6a2af
Naiara Simões <57753296+NaiaraSimoes@users.noreply.github.com>
2022-05-23 12:31:27 +0100

    Add files via upload

--- bisection.m
+++ bisection.m
@@ -1,36 +1,9 @@
-function bisection(f,a,b,kmax,tol,e)
-
-% tabela:
-fprintf('\n   Interação   Valor Calculado ');
-fprintf('\n------------------------------------------\n');
-
-% animação:
-t_pausa = 0.25;
-x = linspace(a,b,100);
-plot(x,f(x));grid on; hold on;
-title('f(x) = ',e);
-xlabel('xx'); ylabel('yy');
-h1 = plot(a, 0, b, 0, 'go', 'MarkerFaceColor', 'g', 'MarkerSize', 8);  
-shg 
-
-% linha preta y = 0
-plot(x, x*0, 'k-');
+function [v, k] = bisection(f,a,b,kmax,tol)
 
 for k = 1:kmax
+
     c = (a+b)/2; % Primeiro ponto médio
-
-    % tabela:
-    fprintf(' %dº interação:     %.6f   %.6f \n',k,c, f(c));
-
-    % animação:
-    pause(t_pausa);
-    delete(h1);
-    h1 = plot(a, 0, 'ro', 'MarkerFaceColor', 'r', 'MarkerSize', 8); 
-    pause(t_pausa);
-    h = plot(c, 0, 'go', 'MarkerFaceColor', 'g', 'MarkerSize', 8);
-    pause(t_pausa);
-    delete(h)
-    h = plot(c, 0, 'ro', 'MarkerFaceColor', 'r', 'MarkerSize', 8);
+    v(k) = c; % vetor com os valores de c para a tabela
 
     % bisection
     if f(c) == 0 % Pára se houver uma raíz
@@ -47,4 +20,6 @@ for k = 1:kmax
     else 
         a = c;
     end
+end
+
 end
